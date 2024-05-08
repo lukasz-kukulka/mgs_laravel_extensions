853dc0d0e24d70d2c1ce506e8a9bc8dab3c386ac
lukasz-kukulka <qqla83@gmail.com>
2024-05-08 11:05:19 +0100

    fixed routes

--- ConfirmAccessExtension/Routes/routes.php
+++ ConfirmAccessExtension/Routes/routes.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::post('confirm/access', [ MGS\ConfirmAccessExtension\Controllers\ConfirmAccessController::class, 'redirect'] )
+Route::post('confirm/access', [ MGSCAE\Controllers\ConfirmAccessController::class, 'redirect'] )
     ->name('mgs_access_question')
     ->middleware( ['auth', 'verified'] );
