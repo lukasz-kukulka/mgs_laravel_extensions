6f12e5b369bdc0886968316182371332889d339a
lukasz-kukulka <qqla83@gmail.com>
2024-05-11 01:16:37 +0100

    blade fixed errors v3

--- ChangePasswordExtension/Views/change_password.blade.php
+++ ChangePasswordExtension/Views/change_password.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -8,35 +9,39 @@
                 @php $translations = include base_path('vendor\mgs\change_password\ChangePasswordExtension\Translations\\' . app()->getLocale() . '\base.php'); @endphp
                 <div class="card-header">{{ $translations['user_account_change_password_title'] }}</div>
                 <div class="card-body">
-                    <form action="{{ route('update_password') }}" method="POST" class="card-body">
-                        @csrf
+                    <div class="row justify-content-center">
+                        <div class="col-md-6">
+                            <form action="{{ route('update_password') }}" method="POST" class="card-body">
+                                @csrf
 
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @elseif (session('error'))
-                            <div class="alert alert-danger" role="alert">
-                                {{ session('error') }}
-                            </div>
-                        @endif
-                        <div class="mb-3">
-                            <label for="new_password" class="form-label">{{ $translations['new_password'] }}</label>
-                            <input name="new_password" type="password" class="form-control @error('new_password') is-invalid @enderror" id="new_password"
-                                placeholder="{{ $translations['new_password'] }}">
-                            @error('new_password')
-                                <span class="text-danger">{{ $message }}</span>
-                            @enderror
+                                @if (session('status'))
+                                    <div class="alert alert-success" role="alert">
+                                        {{ session('status') }}
+                                    </div>
+                                @elseif (session('error'))
+                                    <div class="alert alert-danger" role="alert">
+                                        {{ session('error') }}
+                                    </div>
+                                @endif
+                                <div class="mb-3">
+                                    <label for="new_password" class="form-label">{{ $translations['new_password'] }}</label>
+                                    <input name="new_password" type="password" class="form-control @error('new_password') is-invalid @enderror" id="new_password"
+                                        placeholder="{{ $translations['new_password'] }}">
+                                    @error('new_password')
+                                        <span class="text-danger">{{ $message }}</span>
+                                    @enderror
+                                </div>
+                                <div class="mb-3">
+                                    <label for="new_password_confirmation" class="form-label">{{ $translations['new_password_confirm'] }}</label>
+                                    <input name="new_password_confirmation" type="password" class="form-control" id="new_password_confirmation"
+                                        placeholder="{{ $translations['new_password_confirm'] }}">
+                                </div>
+                                <div class="d-grid">
+                                    <button class="btn btn-success" type="submit">{{ $translations['submit'] }}</button>
+                                </div>
+                            </form>
                         </div>
-                        <div class="mb-3">
-                            <label for="new_password_confirmation" class="form-label">{{ $translations['new_password_confirm'] }}</label>
-                            <input name="new_password_confirmation" type="password" class="form-control" id="new_password_confirmation"
-                                placeholder="{{ $translations['new_password_confirm'] }}">
-                        </div>
-                        <div class="d-grid">
-                            <button class="btn btn-success" type="submit">{{ $translations['submit'] }}</button>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
